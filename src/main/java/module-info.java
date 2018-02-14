cafffb7ff300182ce049e41ae553d2322a803343
Johannes Donath <johannesd@torchmind.com>
2018-02-15 00:29:20 +0100

    Added a Java 9 module definition.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * Provides utility implementations which simplify the parsing and comparing of version numbers and
+ * ranges.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+module com.torchmind.utility.version {
+  exports com.torchmind.utility.version;
+  exports com.torchmind.utility.version.semantic;
+
+  requires static com.github.spotbugs.annotations;
+}
