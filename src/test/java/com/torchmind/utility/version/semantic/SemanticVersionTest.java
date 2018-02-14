78a9137dd0d5d32f13db93e68f214d4a7d5f59e6
Johannes Donath <johannesd@torchmind.com>
2018-02-15 00:05:59 +0100

    Updated copyright notices and updated maven build configuration.

--- src/test/java/com/torchmind/utility/version/semantic/SemanticVersionTest.java
+++ src/test/java/com/torchmind/utility/version/semantic/SemanticVersionTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * Copyright 2018 Johannes Donath <johannesd@torchmind.com>
  * and other copyright owners as documented in the project's IP log.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
